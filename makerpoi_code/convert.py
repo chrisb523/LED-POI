f3cf463e70ec488f2dbd8d8fc700b22856a4d7fb
chrisb523 <chris_butulis@caryacademy.org>
2019-01-27 19:36:41 -0500

    convert.py updated
    Finished correct formatting for header file

--- makerpoi_code/convert.py
+++ makerpoi_code/convert.py
@@ -16,14 +16,25 @@ h,w,d = np.shape(img)
 
 arr = np.asarray(img)
 arr = arr[:, :, :3] #Cuts off extraneous channels 
-size = len(arr)
-i=0
+size = len(arr) #add 1 due to the first init line
+j=0 #counter variable
 
 with open('temp.txt', 'a') as f:
-    for i in range (1,10):
-        i=i+1
-        for i in range (1, h): #Specifiy a second condition for image length
-            print(arr[i,i], file=f)
+
+    print("const uint8_t PROGMEM values[][3] = {", file=f)
+    for i in range (len(arr)):
+        for j in range(1, h): #Specifiy a second condition for image length
+            if j > pixels:
+                break;
+            j=j+1
+            print("{", end="", file=f) 
+            print(arr[i,j], end="", file=f)
+            if j == pixels+1:
+                print("}", end="", file=f)
+            else:
+                print("},", end="", file=f)
+            print("\n", end="", file=f)
+    print("};", file=f)
 
 with open("temp.txt", "rt") as fin: #Remove brackets
     with open("temp1.txt", "wt") as fout:
@@ -41,10 +52,15 @@ with open("temp2.txt", "rt") as fin: #remove double spaces
             fout.write(line.replace('  ', ' '))
 
 with open("temp3.txt", "rt") as fin: #Add commas
+    i=0 #counter variable
     with open("output.txt", "wt") as fout:
         for line in fin:
-            fout.write(line.replace(' ', ', '))
-
+            i+=1
+            if i == 1:
+                fout.write(line)
+            else:
+               fout.write(line.replace(' ', ', '))
+        
 f = open('output.txt')
 file_contents = f.read()
 print(file_contents)
