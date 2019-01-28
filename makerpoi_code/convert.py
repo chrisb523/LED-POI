c760e746741d70f8460ceb310b85d6af7bb3a406
chrisb523 <chris_butulis@caryacademy.org>
2019-01-27 20:47:53 -0500

    convert v0.7.0 finished

--- makerpoi_code/convert.py
+++ makerpoi_code/convert.py
@@ -17,34 +17,38 @@ h,w,d = np.shape(img)
 arr = np.asarray(img)
 arr = arr[:, :, :3] #Cuts off extraneous channels 
 size = len(arr) #add 1 due to the first init line
-j=0 #counter variable
 
 with open('temp.txt', 'a') as f:
-
     print("const uint8_t PROGMEM values[][3] = {", file=f)
     for i in range (len(arr)):
         for j in range(1, h): #Specifiy a second condition for image length
             if j > pixels:
                 break;
-            j=j+1
             print("{", end="", file=f) 
             print(arr[i,j], end="", file=f)
-            if j == pixels+1:
-                print("}", end="", file=f)
-            else:
-                print("},", end="", file=f)
+            print("},", end="", file=f)
             print("\n", end="", file=f)
     print("};", file=f)
 
 with open("temp.txt", "rt") as fin: #Remove brackets
     with open("temp1.txt", "wt") as fout:
+        i=0
         for line in fin:
-            fout.write(line.replace('[', ''))
+            i+=1
+            if i == 1:
+                fout.write(line)
+            else:
+                fout.write(line.replace('[', ''))
 
 with open("temp1.txt", "rt") as fin:
     with open("temp2.txt", "wt") as fout:
+        i=0 
         for line in fin:
-            fout.write(line.replace(']', ''))
+            i+=1
+            if i == 1:
+                fout.write(line)
+            else:
+                fout.write(line.replace(']', ''))
 
 with open("temp2.txt", "rt") as fin: #remove double spaces
     with open("temp3.txt", "wt") as fout:
@@ -53,13 +57,23 @@ with open("temp2.txt", "rt") as fin: #remove double spaces
 
 with open("temp3.txt", "rt") as fin: #Add commas
     i=0 #counter variable
-    with open("output.txt", "wt") as fout:
+    with open("temp4.txt", "wt") as fout:
         for line in fin:
             i+=1
             if i == 1:
                 fout.write(line)
             else:
                fout.write(line.replace(' ', ', '))
+
+with open("temp4.txt", "rt") as fin: #Remove last comma
+    with open("output.txt", "wt") as fout:
+        compare = 0 #Counter variable to compare current iteration to required one
+        for line in fin:
+            compare+=1
+            if compare == (i-1):
+                fout.write(line.replace('},', '}'))
+            else:
+                fout.write(line)
         
 f = open('output.txt')
 file_contents = f.read()
@@ -69,6 +83,7 @@ os.remove("temp.txt")
 os.remove("temp1.txt")
 os.remove("temp2.txt")
 os.remove("temp3.txt")
+os.remove("temp4.txt")
 
             
 
