33de51799e7a8e39178dd4bdd55203c760d9798a
chrisb523 <chris_butulis@caryacademy.org>
2019-04-14 20:50:08 -0400

    v0.8.5

--- makerpoi_code/makerpoi_code.ino
+++ makerpoi_code/makerpoi_code.ino
@@ -7,24 +7,24 @@ Adafruit_NeoPixel strip = Adafruit_NeoPixel(32, PIN, NEO_BRG + NEO_KHZ800);
 
 #include "graphicz.h"
 
-uint8_t  ImageLine = 0;
-uint8_t  ImagePixels = 0;
-uint8_t  cb; //Current byte
-uint8_t  pixelNum = 0; //,
-
 void setup() {
   strip.begin();
   strip.setBrightness(12);
 }
 
 void loop() {
-  int current_pixel = 0;
-  for(int i=0; i<2000; i++) {
-    strip.setPixelColor(current_pixel, values[i][0], values[i][1], values[i][2]);
+  int numColors= sizeof(values)/3; //Gets the number of lines in the array
+  int current_pixel = 0; //Stores the current pixel
+
+  
+  for(int i=0; i<numColors; i++) {
+    strip.setPixelColor(current_pixel, values[i][0], values[i][1], values[i][2]); //Sets the current pixel to the three colors on the given line of the array
     strip.show(); 
+    
     current_pixel++; 
+    
     if (current_pixel == 32)
       current_pixel = 0;
-    delay(2);
   }
 }
+  
