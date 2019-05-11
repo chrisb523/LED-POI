b8d02aa6283fd2e6b04c7de74cbd69ee3debdeb0
chrisb523 <43218982+chrisb523@users.noreply.github.com>
2019-05-10 23:53:38 -0400

    v1.0 Optimized the LED algorithm and added the 3d print model
    Removed all for loops in makerpoi_code.ino and added the 3D model of the poi insert. Additionally, added some test images from themoodhoops packages.

--- makerpoi_code/convert.py
+++ makerpoi_code/convert.py
@@ -1,9 +1,11 @@
-#   Convert Images To Array
+#   Convert Images To Poi Program
 #   Chris Butulis 
-#   Last Updated: 4.7.19
+#   Last Updated: 5.10.19
 #   Python Version: 3.7.2
 
-#**IMPORTANT NOTES: THIS FILE CONVERTS convert.jpg to output.txt**#
+#**IMPORTANT NOTES: THIS FILE CONVERTS convert.jpg to makerpoi_code.ino**#
+#Proper syntax is in shell as follows: $python convert.py convert.jpg > makerpoi_code.ino
+
 import cv2
 import numpy as np
 from PIL import Image
@@ -19,93 +21,61 @@ arr = arr[:, :, :3] #Cuts off extraneous channels
 size = len(arr) #add 1 due to the first init line
 
 with open('temp.txt', 'a') as f:
-    print("uint8_t values[][3] = {", file=f)
-    for i in range (w): #could also use len(arr)?
+    for i in range (w): 
         for j in range(len(arr)): #Specifiy a second condition for image length
             if j > pixels:
                 break;
-            print("{", end="", file=f) 
-            print(arr[j, i], end="", file=f)
-            print("},", end="", file=f)
-            print("\n", end="", file=f)
-    print("};", file=f)
+            print("strip.setPixelColor(",(j+1), "",  arr[j, i], ");", end="\n", file=f)
+        print("strip.show();", end="\n", file=f)
+        print("delay(10);", end="\n", file=f)
+            
 
-with open("temp.txt", "rt") as fin: #Remove brackets
+with open("temp.txt", "rt") as fin: #remove double spaces
     with open("temp1.txt", "wt") as fout:
-        i=0
         for line in fin:
-            i+=1
-            if i == 1:
-                fout.write(line)
-            else:
-                fout.write(line.replace('[', ''))
+            fout.write(line.replace(' [', ''))
 
-with open("temp1.txt", "rt") as fin: #remove brackets
+with open("temp1.txt", "rt") as fin: #remove double spaces
     with open("temp2.txt", "wt") as fout:
-        i=0 
         for line in fin:
-            i+=1
-            if i == 1:
-                fout.write(line)
-            else:
-                fout.write(line.replace(']', ''))
+            fout.write(line.replace('] ', ''))
 
 with open("temp2.txt", "rt") as fin: #remove double spaces
     with open("temp3.txt", "wt") as fout:
         for line in fin:
-            fout.write(line.replace('  ', ' '))
+            fout.write(line.replace(' ', ', '))
 
-with open("temp3.txt", "rt") as fin: #Add commas
-    i=0 #counter variable
-    with open("temp4.txt", "wt") as fout:
+with open("temp3.txt", "rt") as fin: 
+    with open("temp4.txt", "wt") as fout: 
         for line in fin:
-            i+=1
-            if i == 1:
-                fout.write(line)
-            else:
-               fout.write(line.replace(' ', ', '))
+            fout.write(line.replace('(, ', '('))
 
-with open("temp4.txt", "rt") as fin: #Remove brackets
-    with open("temp5.txt", "wt") as fout:
-        i=0
+with open("temp4.txt", "rt") as fin: 
+    with open("temp5.txt", "wt") as fout: 
         for line in fin:
-            i+=1
-            if i == 1:
-                fout.write(line)
-            else:
-                fout.write(line.replace('{, ', '{'))               
+            fout.write(line.replace(', , , ', ', '))
 
-with open("temp5.txt", "rt") as fin: #Remove brackets
-    with open("temp6.txt", "wt") as fout:
-        i=0
+with open("temp5.txt", "rt") as fin: 
+    with open("temp6.txt", "wt") as fout: 
         for line in fin:
-            i+=1
-            if i == 1:
-                fout.write(line)
-            else:
-                fout.write(line.replace(', , ', ', '))   
+            fout.write(line.replace(', , ', ', '))
+                   
+with open("temp6.txt", "rt") as fin: 
+    with open("output.txt", "wt") as fout: 
+        fout.write("#include <Adafruit_NeoPixel.h>\n#define PIN 1\n#define NUM_LEDS 32\nAdafruit_NeoPixel strip = Adafruit_NeoPixel(32, PIN, NEO_GRB + NEO_KHZ800);\nvoid setup() {\nstrip.begin();\nstrip.setBrightness(40);\n}\nvoid loop() {\n")
+        fout.write(fin.read())
+        fout.write("}")
 
-with open("temp6.txt", "rt") as fin: #Remove last comma
-    with open("output.txt", "wt") as fout:
-        compare = 0 #Counter variable to compare current iteration to required one
-        for line in fin:
-            compare+=1
-            if compare == (i-1):
-                fout.write(line.replace('},', '}'))
-            else:
-                fout.write(line)
-        
 f = open('output.txt')
 file_contents = f.read()
 print(file_contents)
 
-os.remove("temp.txt")
-os.remove("temp1.txt")
-os.remove("temp2.txt")
-os.remove("temp3.txt")
-os.remove("temp4.txt")
-os.remove("temp5.txt")
-os.remove("temp6.txt")            
-
+os.remove('temp.txt')
+os.remove('temp1.txt')
+os.remove('temp2.txt')
+os.remove('temp3.txt')
+os.remove('temp4.txt')
+os.remove('temp5.txt')         
+os.remove('temp6.txt')
 
     
